7fbe957ae8a84377a9c5eafb150be6e62dd668a5
ihsanuddienullah <ihsanuddienullah@gmail.com>
2021-10-11 00:01:05 +0700

    controllers

--- server.js
+++ server.js
@@ -5,13 +5,15 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
+// bring routes
+const blogRoutes = require("./routes/blog");
 
 // app
 const app = express();
 
 // db
 mongoose
-    .connect(process.env.DATABASE, {
+    .connect(process.env.DATABASE_LOCAL, {
         useNewUrlParser: true,
     })
     .then(() => console.log("DB Connected"));
@@ -24,11 +26,8 @@ app.use(cookieParser());
 if (process.env.NODE_ENV === "development") {
     app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
 }
-
-// routes
-app.get("/api", (req, res) => {
-    res.json({ time: Date().toString() });
-});
+// routes middleware
+app.use(blogRoutes)
 
 // port
 const port = process.env.PORT || 8000;
