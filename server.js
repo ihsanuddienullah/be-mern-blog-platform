e0fb23fe7f6317b64a90e508349bc4894569f7a9
ihsanuddienullah <ihsanuddienullah@gmail.com>
2021-10-20 22:27:58 +0700

    Signup validator

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
+
 // bring routes
 const blogRoutes = require("./routes/blog");
+const authRoutes = require("./routes/auth");
 
 // app
 const app = express();
@@ -22,12 +24,15 @@ mongoose
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
+
 // cors
 if (process.env.NODE_ENV === "development") {
     app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
 }
+
 // routes middleware
-app.use(blogRoutes)
+app.use("/api", blogRoutes);
+app.use("/api", authRoutes);
 
 // port
 const port = process.env.PORT || 8000;
