e2799d35aaa5d26a3188d44719778e77b1e4afa6
ihsanuddienullah <ihsanuddienullah@gmail.com>
2021-12-05 23:14:03 +0700

    80. List all blogs categories and tags

--- controllers/blog.js
+++ controllers/blog.js
@@ -39,7 +39,7 @@ exports.create = (req, res) => {
             });
         }
 
-        if (!tag || tag.length === 0) {
+        if (!tags || tags.length === 0) {
             return res.status(404).json({
                 error: "At least one tag is required",
             });
@@ -56,7 +56,7 @@ exports.create = (req, res) => {
 
         // categories and tags
         let arrayOfCategories = categories && categories.split(",");
-        let arrayOfTagss = tagss && tagss.split(",");
+        let arrayOfTags = tags && tags.split(",");
 
         if (files.photo) {
             if (files.photo.size > 10000000) {
@@ -102,4 +102,59 @@ exports.create = (req, res) => {
             });
         });
     });
+};
+
+exports.list = (req, res) => {
+    Blog.find({})
+        .populate("categories", "_id name slug")
+        .populate("tags", "_id name slug")
+        .populate("postedBy", "_id name username")
+        .select(
+            "_id title slug excerpt categories tags postedBy createdAt updatedAt"
+        )
+        .exec((err, data) => {
+            if (err) {
+                return res.json({ error: errorHandler(err) });
+            }
+            res.json(data);
+        });
+};
+
+exports.listAllBlogsCategoriesTags = (req, res) => {
+    let limit = req.body.limit ? parseInt(req.body.limit) : 10;
+    let skip = req.body.skip ? parseInt(req.body.skip) : 0;
+
+    let blogs;
+    let categories;
+    let tags;
+
+    Blog.find({})
+        .populate("categories", "_id name slug")
+        .populate("tags", "_id name slug")
+        .populate("postedBy", "_id name username")
+        .sort({ createdAt: -1 })
+        .skip(skip)
+        .limit(limit)
+        .select(
+            "_id title slug excerpt categories tags postedBy createdAt updatedAt"
+        )
+        .exec((err, data) => {
+            if (err) {
+                return res.json({ error: errorHandler(err) });
+            }
+            blogs = data;
+            Category.find({}).exec((err, c) => {
+                if (err) {
+                    return res.json({ error: errorHandler(err) });
+                }
+                categories = c;
+                Tag.find({}).exec((err, t) => {
+                    if (err) {
+                        return res.json({ error: errorHandler(err) });
+                    }
+                    tags = t;
+                    res.json({ blogs, categories, tags, size: blogs.length });
+                });
+            });
+        });
 };
