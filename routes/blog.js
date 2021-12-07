6e114533d346b17bae112d417aa88233171e1eea
ihsanuddienullah <ihsanuddienullah@gmail.com>
2021-12-07 09:36:48 +0700

    83. Show blog photo

--- routes/blog.js
+++ routes/blog.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { create, listAllBlogsCategoriesTags, list, read, remove, update } = require("../controllers/blog");
+const { create, listAllBlogsCategoriesTags, list, read, remove, update, photo } = require("../controllers/blog");
 const { requireSignin, adminMiddleware } = require("../controllers/auth");
 
 router.post("/blog", requireSignin, adminMiddleware, create);
 router.post("/blogs-categories-tags", listAllBlogsCategoriesTags);
 router.get("/blogs", list);
+router.get("/blog/photo/:slug", photo);
 router.get("/blog/:slug", read);
 router.delete("/blog/:slug", requireSignin, adminMiddleware, remove);
 router.put("/blog/:slug", requireSignin, adminMiddleware, update);
