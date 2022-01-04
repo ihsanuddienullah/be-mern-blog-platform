708c4eaa87204510694a1dfe5af2784a3ef18788
Muhammad Ihsanuddienullah <Muhammad.Ihsanuddienullah@xapiens.id>
2022-01-04 23:48:47 +0700

    120. Profile update component setup

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,9 @@
 const User = require("../models/user");
 const Blog = require("../models/blog");
 const { errorHandler } = require("../helpers/dbErrorHandler");
+const formidable = require("formidable");
+const _ = require("lodash");
+const fs = require("fs");
 
 exports.read = (req, res) => {
     req.profile.hashed_password = undefined;
@@ -37,5 +40,52 @@ exports.publicProfile = (req, res) => {
                 user.hashed_password = undefined;
                 res.json({ user, blogs: data });
             });
+    });
+};
+
+exports.update = (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            return res.status(400).json({
+                error: "Photo could not be uploaded",
+            });
+        }
+        let user = req.profile;
+        user = _.extend(user, fields);
+
+        if (files.photo) {
+            if (files.photo.size > 100000) {
+                return res.status(400).json({
+                    error: "Image should be less than 1mb",
+                });
+            }
+            user.photo.data = fs.readFileSync(files.photo.path);
+            user.photo.contentType = files.photo.type;
+
+            user.save((err, result) => {
+                if (err) {
+                    return res.status(err).json({ error: errorHandler(err) });
+                }
+                user.hashed_password = undefined;
+                res.json(result);
+            });
+        }
+    });
+};
+
+exports.photo = (req, res) => {
+    const username = req.params.username;
+    User.findOne({ username }).exec((err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: "User not found",
+            });
+        }
+
+        if (user.photo.data) {
+            res.set("Content-Type", user.photo.contentType);
+            return res.send(user.photo.data);
+        }
     });
 };
